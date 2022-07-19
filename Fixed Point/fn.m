516148cbdf6906533f4de08d0574d8f3e55abdd2
SadMadLad <74383721+SadMadLad@users.noreply.github.com>
2022-07-19 18:16:43 +0500

    Add files via upload

---
+++ Fixed Point/fn.m
@@ -0,0 +1,6 @@
+function [fx] = fn(x)
+% returns g(x) of the given input.
+% declare the function here
+fx = x^(2/3);
+end
+
---
+++ Fixed Point/fn_dash.m
@@ -0,0 +1,6 @@
+function [f_dash_x] = fn_dash(x)
+%   g'(c) of x = c
+%   declare the first derivative of function here
+f_dash_x = (2/3)*x^(2/3 - 1);
+end
+
